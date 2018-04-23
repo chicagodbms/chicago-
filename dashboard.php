ceac06a70f008f4b786c98a7bc486b511089b74d
GrandStar <gzqsjtu@163.com>
2018-04-22 20:27:48 -0400

    chart

--- dashboard.php
+++ dashboard.php
@@ -13,12 +13,17 @@
 Online Crime Database | Login
 </title>
 
-    <link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+<!--    <link rel="stylesheet" href="css/style.css" type="text/css" media="all" />-->
+    <link rel="stylesheet" href="css/stylemain.css" type="text/css" media="all" />
+    <script src="js/jquery-1.4.1.min.js" type="text/javascript"></script>
+    <script src="js/jquery.jcarousel.pack.js" type="text/javascript"></script>
+    <script src="js/jquery.slide.js" type="text/javascript"></script>
+    <script src="js/jquery-func.js" type="text/javascript"></script>
 <!--    <script src="js/jquery-1.4.1.min.js" type="text/javascript"></script>-->
 <!--    <script src="js/jquery.jcarousel.pack.js" type="text/javascript"></script>-->
 <!--    <script src="js/jquery.slide.js" type="text/javascript"></script>-->
 <!--    <script src="js/jquery-func.js" type="text/javascript"></script>-->
-    <link rel="stylesheet" href="css/style_mainstore.css" type="text/css" media="all" />
+<!--    <link rel="stylesheet" href="css/style_mainstore.css" type="text/css" media="all" />-->
     <style>
         /* Always set the map height explicitly to define the size of the div
          * element that contains the map. */
@@ -32,22 +37,38 @@ Online Crime Database | Login
             /*padding: 0;*/
         /*}*/
     </style>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="Trendy Charts Widget Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+        function hideURLbar(){ window.scrollTo(0,1); } </script>
+    <!-- //Custom-Theme-files -->
+    <!-- js -->
+    <script src="js/jquery.min.js"></script>
+    <!-- //js -->
+    <link rel="stylesheet" type="text/css" href="css/jqcandlestick.css" />
+    <!-- area-chart -->
+    <link rel="stylesheet" href="css/morris.css">
+    <!-- //area-chart -->
+    <link href="css/datachartstyle.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="//fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese" rel="stylesheet">
+
 </head>
 
 <body style="background-color: beige">
 
 <div id="top">
-    <div class="shell">
-        <div id="header">
-            <h1 id="logo"><a href="login.php">Online Store</a></h1>
-            <div id="navigation">
-                <ul>
-                    <li><a href="login.php">Home</a></li>
-                    <li><a href="about.php">About the Store</a></li>
-                    <li><a href="contact.php">Contact</a></li>
-                    <li><a href="logout.php">Logout</a><li>
-                </ul>
-            </div>
+    <!--    <div class="shell">-->
+    <div>
+        <h1 id="logo"><a href="login.php">Online Database</a></h1>
+        <div id="navigation">
+            <ul>
+                <li><a href="login.php">Home</a></li>
+                <li><a href="about.php">About the Database</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
         </div>
     </div>
 </div>
@@ -62,14 +83,16 @@ Online Crime Database | Login
 //    ?>
 <!--</div>-->
 
-<div id="menu" style="background-color:#bcbcbc;height:800px;width: 29%;float:left">
+<div style="10px">&nbsp</div>
+
+<div id="menu" style="background-color:#bcbcbc;height:800px;width: 30%;float:left">
 <!--    <p>database Operation</p>-->
 </div>
 
 <div style="height:600px;width: 0.5%;float:left">
 <!--    <p>database Operation</p>-->
 </div>
-<div id="content" style="height:800px;width:57%;px;float:left">
+<div id="content" style="height:800px;width:47%;px;float:left">
 <!--     style="background-color:beige;-->
 
 <!--    <p>content</p>-->
@@ -77,9 +100,57 @@ Online Crime Database | Login
     <>
 </div>
 
-<div id="chart" style="background-color:#bcbcbc; height:800px;width: 13%;px;float:right">
+    <div id="chart" style="background-color:#bcbcbc; height:800px;width: 22%;px;float:right">
+        <div class="main">
+<!--            <div class="w3_agile_main_grids">-->
+<!--            <div class="w3_agile_main_grid_left">-->
+                <div class="agile_donut_chart agileits_w3layouts_text">
+<!--                    <h3>Donut / pie-chart</h3>-->
+                    <div class="exp"></div>
+<!--                </div>-->
+<!--            </div>-->
+        </div>
+    </div>
 <!--    <p>layer and chart</p>-->
 </div>
+
+<script src="js/d3.min.js"></script>
+<script src="js/donut-pie-chart.min.js"></script>
+<script type="text/javascript">
+
+    $( document ).ready(function() {
+
+        /**
+         [
+         {"name" : "JavaScript", "hvalue" : 20 },
+         {"name" : "d3", "hvalue" : 2},
+         {"name" : "jQuery", "hvalue" : 25},
+         {"name" : "SVG", "hvalue" : 5}
+         ]
+         */
+        function get_data() {
+            var props = ['crime1', "crime2", "crime3", "crime4"];
+            var out   = [];
+            for (var i = 0; i < props.length; i++) {
+                out.push({"name":props[i], "hvalue": Math.floor(Math.random() * 100)});
+            };
+            return out;
+        }
+
+        // init the pie
+        $(".exp").donutpie();
+
+        // update it with some fake data
+        $(".exp").donutpie('update', get_data());
+
+        // keep updating every x sec
+        setInterval( function () {
+            $(".exp").donutpie('update', get_data());
+        }, 1200);
+
+    });
+
+</script>
 
 
 <script>
